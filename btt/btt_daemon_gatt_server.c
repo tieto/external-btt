e0a233d669139554295471edf6721ed3e74b9537
Wojciech Wojciechowski <ext.wojciech.wojciechowski@tieto.com>
2014-08-25 11:33:20 +0200

    Register server added
    Change-Id: Id8d199769b923278cda5b18d6e4f892fb671308f

--- btt/btt_daemon_gatt_server.c
+++ btt/btt_daemon_gatt_server.c
@@ -16,22 +16,59 @@
 
 #include "btt.h"
 #include "btt_daemon_gatt_server.h"
+#include "btt_gatt_server.h"
+#include "btt_utils.h"
 
 #include <hardware/bt_gatt.h>
+
+extern const btgatt_server_interface_t *gatt_server_if;
+extern int socket_remote;
 
 void handle_gatt_server_cmd(const struct btt_message *btt_msg,
 		const int socket_remote)
 {
-	switch (btt_msg->command)
+	switch (btt_msg->command) {
+	case BTT_GATT_SERVER_CMD_REGISTER_SERVER:
+	{
+		struct btt_gatt_server_reg msg;
+
+		if (!RECV(&msg, socket_remote)) {
+			BTT_LOG_E("Received invalid btt_gatt_server_reg\n");
+			close(socket_remote);
+			return;
+		}
+
+		gatt_server_if->register_server(&msg.UUID);
+		break;
+	}
 	default: break;
+	}
 }
 
 /*************************************************************/
 /* Gatt server: callbacks, necessary functions and structure */
 /*************************************************************/
 
+static void register_server_cb(int status, int server_if, bt_uuid_t *app_uuid)
+{
+	struct btt_gatt_server_cb_reg_result btt_cb;
+
+	btt_cb.hdr.type = BTT_GATT_SERVER_CB_REGISTER_SERVER;
+	btt_cb.hdr.length = sizeof(struct btt_gatt_server_cb_reg_result)
+			- sizeof(struct btt_gatt_server_cb_hdr);
+	btt_cb.status = status;
+	btt_cb.server_if = server_if;
+	memcpy(&btt_cb.app_uuid, app_uuid, sizeof(bt_uuid_t));
+
+	BTT_LOG_D("Callback_GS Server Register");
+
+	if (send(socket_remote, &btt_cb,
+			sizeof(struct btt_gatt_server_cb_reg_result), 0) == -1)
+		 BTT_LOG_E("%s:System Socket Error\n", __FUNCTION__);
+}
+
 static btgatt_server_callbacks_t sGattServerCallbacks = {
-		NULL,
+		register_server_cb,
 		NULL,
 		NULL,
 		NULL,
--- btt/btt_gatt_server.c
+++ btt/btt_gatt_server.c
@@ -21,10 +21,11 @@
 #define MAX_ARGC 20
 
 static void run_gatt_server_help(int argc, char **argv);
+static void run_gatt_server_reg(int argc, char **argv);
 
 static const struct extended_command gatt_server_commands[] = {
 		{{ "help",							"",						run_gatt_server_help}, 1, MAX_ARGC},
-		{{ "register_server",				"NOT IMPLEMENTED YET",	NULL				}, 1, 1},
+		{{ "register_server",				"<16-bits UUID>",		run_gatt_server_reg}, 2, 2},
 		{{ "unregister_server",				"NOT IMPLEMENTED YET",	NULL				}, 1, 1},
 		{{ "connect",						"NOT IMPLEMENTED YET",	NULL				}, 1, 1},
 		{{ "disconnect",					"NOT IMPLEMENTED YET",	NULL				}, 1, 1},
@@ -50,7 +51,112 @@ void run_gatt_server_help(int argc, char **argv)
 
 static void process_request(enum btt_gatt_server_req_t type, void *data)
 {
-	/* TODO: add function's body */
+	int server_sock;
+	unsigned int len;
+	struct sockaddr_un server;
+	struct btt_message msg;
+	struct timeval tv;
+	struct btt_gatt_server_cb_hdr btt_cb;
+	char *buffer;
+	unsigned int i;
+
+	errno = 0;
+
+	if ((server_sock = socket(AF_UNIX, SOCK_STREAM, 0)) == -1)
+		return;
+
+	server.sun_family = AF_UNIX;
+
+	strcpy(server.sun_path, SOCK_PATH);
+
+	len = strlen(server.sun_path) + sizeof(server.sun_family);
+
+	if (connect(server_sock, (struct sockaddr *)&server, len) == -1) {
+		close(server_sock);
+		return;
+	}
+
+	switch (type) {
+	case BTT_GATT_SERVER_REQ_END:
+		break;
+	case BTT_GATT_SERVER_REQ_REGISTER_SERVER:
+	{
+		struct btt_gatt_server_reg *register_server =
+				(struct btt_gatt_server_reg *) data;
+
+		register_server->hdr.command = BTT_GATT_SERVER_CMD_REGISTER_SERVER;
+		register_server->hdr.length = sizeof(struct btt_gatt_server_reg)
+				- sizeof(struct btt_message);
+
+		if (send(server_sock, register_server,
+				sizeof(struct btt_gatt_server_reg), 0) == -1) {
+			close(server_sock);
+			return;
+		}
+
+		break;
+	}
+	default:
+		break;
+	}
+
+	len = 0;
+
+	while (1) {
+		len = recv(server_sock, &btt_cb, sizeof(btt_cb), MSG_PEEK);
+
+		if (len == 0 || errno) {
+			BTT_LOG_S("Timeout\n");
+			close(server_sock);
+			return;
+		}
+        /* here we receive all messages on the socket. But only requested
+         * messages are printed (i.e. type == BTT_REQ_AGENT)
+         */
+		switch (btt_cb.type) {
+		case BTT_GATT_SERVER_CB_REGISTER_SERVER:
+		{
+			struct btt_gatt_server_cb_reg_result cb;
+
+			memset(&cb, 0, sizeof(cb));
+
+			if (!RECV(&cb,server_sock)) {
+				BTT_LOG_S("Error: incorrect size of received structure.\n");
+				return;
+			}
+
+			if (type == BTT_GATT_SERVER_REQ_REGISTER_SERVER) {
+				BTT_LOG_S("\n");
+				printf_UUID_128(cb.app_uuid.uu, FALSE);
+				BTT_LOG_S("Status: %s\n",!cb.status ? "OK" : "ERROR");
+				BTT_LOG_S("Server interface: %d\n\n", cb.server_if);
+			}
+
+			return;
+		}
+		default:
+			buffer = malloc(btt_cb.length);
+
+			if (buffer) {
+				recv(server_sock, buffer, btt_cb.length, 0);
+				free(buffer);
+			}
+
+			break;
+		}
+	}
+}
+
+static void run_gatt_server_reg(int argc, char **argv)
+{
+	struct btt_gatt_server_reg req;
+
+	if (!sscanf_UUID(argv[1], req.UUID.uu)) {
+			BTT_LOG_S("Error: Incorrect UUID\n");
+			return;
+	}
+
+	process_request(BTT_GATT_SERVER_REQ_REGISTER_SERVER, &req);
 }
 
 void run_gatt_server(int argc, char **argv)
