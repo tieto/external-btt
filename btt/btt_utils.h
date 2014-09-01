8ea45cc6cec5d08fc2a78179e1cc8ff3d4c53120
Aleksander Drewnicki <ext.aleksander.drewnicki@tieto.com>
2014-09-01 15:37:46 +0200

    Modify printf_UUID_128
    Add inverted UUID printing possibility
    
    Change-Id: I731710dad572340b5a1f8a6c608cc7fc89dcdc0a

--- btt/btt_utils.h
+++ btt/btt_utils.h
@@ -49,7 +49,7 @@ struct list_element *list_clear(struct list_element *list,
 extern bool sscanf_bdaddr(char *src, uint8_t *dest);
 extern int string_to_hex(char *src, uint8_t *dest);
 extern bool sscanf_UUID(char *src, uint8_t *dest);
-extern void printf_UUID_128(uint8_t *src);
+extern void printf_UUID_128(uint8_t *src, bool invert);
 
 /* return FALSE if length of received structure is different
  * from expected length */
